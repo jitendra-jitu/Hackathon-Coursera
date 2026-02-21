1a9328afdd0b3789495f9ba0aa1738d3d2a31386
Gudela <2464352@cognizant.com>
2026-02-21 16:18:38 +0530

    Github actions..

---
+++ jenkins.groovy
@@ -0,0 +1,51 @@
+pipeline {
+    agent any
+ 
+    stages {
+        
+        // 1 fecthing project from git repository
+        stage('Checkout') {
+            steps {
+                git branch: 'main',
+                    url: 'https://github.com/jitendra-jitu/Hackathon-Coursera'
+            }
+        }
+ 
+        // 2 installing dependencies
+        stage('Install Dependencies') {
+            steps {
+                bat 'npm install'
+                bat 'npm install -D allure-playwright'
+                // bat 'npm install -g allure-commandline --force'
+            }
+        }
+ 
+
+        // 3 running tests
+        stage('Run Tests') {
+            steps {
+                bat 'npx playwright install'
+                bat 'npx playwright test'         
+            }
+        }
+ 
+        // stage('Generate Allure Report') {
+        //     steps {
+        //         bat 'allure generate ./allure-results --clean -o ./allure-report'
+        //     }
+        // }
+    }
+ 
+    post {
+        always {
+
+            archiveArtifacts artifacts: 'test-results/**/*', allowEmptyArchive:
+true
+ 
+ 
+            allure includeProperties: false,
+                   jdk: '',
+                   results: [[path: 'allure-results']]
+        }
+    }
+}
